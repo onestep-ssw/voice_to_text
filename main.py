2c9868da31fff26568f165d57b18d9ef8e705381
Ssw <sunshiwen@fkmalls.com>
2023-10-15 11:03:51 +0800

    fix

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import os
 import handler.ui_handler as ui_handel
 
 os.environ["ffmpeg"] = os.getcwd() + "/ffmpeg/ffmpeg-master-latest-win64-gpl/bin/"
-model_path = os.getcwd() + "/vosk-model-cn-0.22"
+model_path = os.getcwd() + "\\vosk-model-cn-0.22"
 
 
 
 
 if __name__ == '__main__':
-     ui_handel.main()
+     ui_handel.main(model_path)
 
